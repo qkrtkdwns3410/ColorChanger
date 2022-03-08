ba34e3f3678801e6c56d0bc06cb1b225a50172bb
letscombine <qkrtkdwns3410@naver.com>
2022-03-08 12:30:05 +0900

    changes:

--- app/src/main/java/com/example/colorchanger/MainActivity.java
+++ app/src/main/java/com/example/colorchanger/MainActivity.java
@@ -11,6 +11,7 @@ import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -23,17 +24,113 @@ import androidx.viewpager2.widget.ViewPager2;
 
 public class MainActivity extends AppCompatActivity {
 	  
+	  public static int R_VALUE = 0;
+	  public static int G_VALUE = 0;
+	  public static int B_VALUE = 0;
+	  
+	  OnSwipeTouchListener onSwipeTouchListener;
+	  public FrameLayout container;
+	  
 	  @Override
 	  protected void onCreate(Bundle savedInstanceState) {
 			super.onCreate(savedInstanceState);
 			setContentView(R.layout.activity_main);
 			
-			// definition of pager using method findViewById()
-			pager = findViewById(R.id.pager);
+			container = findViewById(R.id.container);
 			
-			// calling the constructor of viewPager class
-			viewPager = new viewPager(getSupportFragmentManager(), 1);
-			pager.setAdapter(viewPager);
+			onSwipeTouchListener = new OnSwipeTouchListener(this, findViewById(R.id.container));
+	  }
+	  
+	  public static class OnSwipeTouchListener implements View.OnTouchListener {
+			private final GestureDetector gestureDetector;
+			Context context;
+			
+			OnSwipeTouchListener(Context ctx, View mainView) {
+				  gestureDetector = new GestureDetector(ctx, new GestureListener());
+				  mainView.setOnTouchListener(this);
+				  context = ctx;
+			}
+			
+			@Override
+			public boolean onTouch(View v, MotionEvent event) {
+				  return gestureDetector.onTouchEvent(event);
+			}
+			
+			public class GestureListener extends
+				GestureDetector.SimpleOnGestureListener {
+				  private static final int SWIPE_THRESHOLD = 100;
+				  private static final int SWIPE_VELOCITY_THRESHOLD = 100;
+				  
+				  @Override
+				  public boolean onDown(MotionEvent e) {
+						return true;
+				  }
+				  
+				  @Override
+				  public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+						boolean result = false;
+						try {
+							  float diffY = e2.getY() - e1.getY();
+							  float diffX = e2.getX() - e1.getX();
+							  if (Math.abs(diffX) > Math.abs(diffY)) {
+									if (Math.abs(diffX) > SWIPE_THRESHOLD && Math.abs(velocityX) > SWIPE_VELOCITY_THRESHOLD) {
+										  if (diffX > 0) {
+												onSwipeRight();
+												
+										  } else {
+												onSwipeLeft();
+										  }
+										  
+										  result = true;
+									}
+							  } else if (Math.abs(diffY) > SWIPE_THRESHOLD && Math.abs(velocityY) > SWIPE_VELOCITY_THRESHOLD) {
+									if (diffY > 0) {
+										  onSwipeBottom();
+									} else {
+										  onSwipeTop();
+									}
+									result = true;
+							  }
+						} catch (Exception exception) {
+							  exception.printStackTrace();
+						}
+						return result;
+				  }
+			}
+			
+			void onSwipeRight() {
+				  Toast.makeText(context, "Swiped Right", Toast.LENGTH_SHORT).show();
+				  container.setBackgroundColor();
+				  this.onSwipe.swipeRight();
+				  
+			}
+			
+			void onSwipeLeft() {
+				  Toast.makeText(context, "Swiped Left", Toast.LENGTH_SHORT).show();
+				  this.onSwipe.swipeLeft();
+			}
+			
+			void onSwipeTop() {
+				  Toast.makeText(context, "Swiped Up", Toast.LENGTH_SHORT).show();
+				  this.onSwipe.swipeTop();
+			}
+			
+			void onSwipeBottom() {
+				  Toast.makeText(context, "Swiped Down", Toast.LENGTH_SHORT).show();
+				  this.onSwipe.swipeBottom();
+			}
+			
+			interface onSwipeListener {
+				  void swipeRight();
+				  
+				  void swipeTop();
+				  
+				  void swipeBottom();
+				  
+				  void swipeLeft();
+			}
+			
+			onSwipeListener onSwipe;
 	  }
 	  
 }
